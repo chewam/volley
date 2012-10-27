faa612e3a694447fc2685bc6b5368f18bc6a3bae
goldledoigt <gary@revolunet.com>
2012-10-27 23:01:19 +0200

    player names

--- js/player.js
+++ js/player.js
@@ -4,7 +4,6 @@ var Player = function(config) {
     this.x *= this.zoom;
     this.y *= this.zoom;
     this.radius *= this.zoom;
-
 };
 
 Player.prototype.getShape = function() {
@@ -13,17 +12,24 @@ Player.prototype.getShape = function() {
         draggable: true
     });
 
-    var line = new Kinetic.Line({
-        points: [
-            this.x + this.radius * 2, this.y + this.radius * 2,
-            this.x + this.radius, this.y + this.radius,
-            this.x, this.y + this.radius * 2
-        ],
-        stroke: this.stroke,
-        strokeWidth: this.strokeWidth
+    // var line = new Kinetic.Line({
+    //     points: [
+    //         this.x + this.radius * 2, this.y + this.radius * 2,
+    //         this.x + this.radius, this.y + this.radius,
+    //         this.x, this.y + this.radius * 2
+    //     ],
+    //     stroke: this.stroke,
+    //     strokeWidth: this.strokeWidth
+    // });
+
+    this.firstMove = new Kinetic.Line({
+        visible: false,
+        stroke: '#666',
+        strokeWidth: this.strokeWidth,
+        dashArray: [10, 5]
     });
 
-    var circle = new Kinetic.Circle({
+    this.circle = new Kinetic.Circle({
         x: this.x + this.radius,
         y: this.y + this.radius,
         radius: this.radius,
@@ -32,7 +38,7 @@ Player.prototype.getShape = function() {
         strokeWidth: this.strokeWidth
     });
 
-    var text = new Kinetic.Text({
+    var number = new Kinetic.Text({
         x: this.x - 7 + this.radius,
         y: this.y - 10 + this.radius,
         text: this.number,
@@ -40,9 +46,18 @@ Player.prototype.getShape = function() {
         textFill: this.textFill
     });
 
-    this.group.add(line);
-    this.group.add(circle);
-    this.group.add(text);
+    this.role = new Kinetic.Text({
+        x: this.x - 15,
+        y: this.y - 15,
+        fontSize: 10,
+        textFill: 'black'
+    });
+
+    this.group.add(this.firstMove);
+    // this.group.add(line);
+    this.group.add(this.circle);
+    this.group.add(this.role);
+    this.group.add(number);
 
     this.group.on("mouseover", function() {
         document.body.style.cursor = "pointer";
@@ -60,16 +75,32 @@ Player.prototype.getShape = function() {
 };
 
 Player.prototype.setPosition = function(position) {
-    _group = this.group;
-    console.log('setPosition', this.number, '-', position.x, position.y, '***', this.x, this.y, '***', position.x - this.x, position.y - this.y);
 
     this.group.transitionTo({
-        x: position.x/* - this.x*/,
-        y: position.y/* - this.y*/,
+        x: position.x,
+        y: position.y,
         easing: 'linear',
         duration: 1
     });
 
     this.x = position.x;
     this.y = position.y;
+
+    if (position.firstMove) {
+        this.firstMove.setPoints([
+            this.radius, this.radius,
+            position.firstMove.x - position.x, position.firstMove.y - position.y
+        ]);
+        this.firstMove.show();
+    } else {
+        this.firstMove.hide();
+    }
+
+    if (position.role === 'setter') {
+        this.circle.setFill('black');
+    } else {
+        this.circle.setFill(this.fill);
+    }
+
+    this.role.setText((position.role || '') + (position.players ? ' - ' + position.players.join(', ') : ''));
 };
